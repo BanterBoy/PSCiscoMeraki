3bcfcb6d7217f08a9b52a332c9900bc87c801b0b
Luke Leigh <luke.leigh@gmail.com>
2019-11-09 23:33:46 +0000

    Update Get-MerakiAccessPolicy.ps1

--- PSCiscoMeraki/Public/Get-MerakiAccessPolicy.ps1
+++ PSCiscoMeraki/Public/Get-MerakiAccessPolicy.ps1
@@ -1,5 +1,4 @@
-function Get-MerakiAccessPolicy {
-    <#
+<#
     .SYNOPSIS
     Short function to provide details for an access policy configured on a specific network ID. Only valid for MS networks.
     In order to use this Module you will need an API Key from your Dashboard.
@@ -7,16 +6,21 @@ function Get-MerakiAccessPolicy {
     .DESCRIPTION
     Short function to provide details for an access policy configured on a specific network ID. Only valid for MS networks.
 
-    This function queries the Cisco Meraki API service https://dashboard.meraki.com/api/v0 and will be needed for use with additional
-    commands within the module.
+    This function queries the Cisco Meraki API service <https://dashboard.meraki.com/api/v0> and will be needed for use with additional commands within the module.
 
     API Access is free but Rate Controlled and is limited to 5 calls per second (per organisation).
 
-    In order to use this Module you will need an API Key from your Dashboard. For access to the API, first enable the API for your
-    organisation under Organiation > Settings > Dashboard API access.
+    In order to use this Module you will need an API Key from your Dashboard. For access to the API, first enable the API for your organisation under Organiation > Settings > Dashboard API access.
 
-    After enabling the API, go to the my profile page to generate an API key. The API key is associated with a Dashboard administrator account.
-    You can generate, revoke, and regenerate your API key on your profile.
+    ![Enable API Access](https://raw.githubusercontent.com/BanterBoy/CiscoMeraki/master/assets/EnableAPIAccess.png)
+
+    After enabling the API, go to the **my profile** page to generate an API key. The API key is associated with a Dashboard administrator account. You can generate, revoke, and regenerate your API key on your profile.
+
+    ![Generate API Key](https://raw.githubusercontent.com/BanterBoy/CiscoMeraki/master/assets/GenerateKey.png)
+
+    ****Note:*** Keep your API key safe as it provides authentication to all of your organizations with the API enabled. If your API key is shared, you can regenerate your API key at any time. This will revoke the existing API key.*
+
+    ****Note*** that SAML dashboard administrators cannot view or generate API keys.*
 
     .EXAMPLE
     Get-MerakiNetworks -ApiKey APIKeyGoesHere -NetworkID NetIDGoesHere
@@ -25,7 +29,7 @@ function Get-MerakiAccessPolicy {
     .INPUTS
     Accepts Api Key as piped input.
     Accepts Organisation ID as piped input.
-
+    
     .OUTPUTS
     The output from the API is sent as JSON and captured in a custom object.
     [
@@ -35,20 +39,20 @@ function Get-MerakiAccessPolicy {
             "accessType": "8021.x",
             "guestVlan": 3700,
             "radiusServers": [
-            {
-                "ip": "1.2.3.4",
-                "port": 1337
-            },
-            {
-                "ip": "2.3.4.5",
-                "port": 1337
-            }
+                {
+                    "ip": "1.2.3.4",
+                    "port": 1337
+                },
+                {
+                    "ip": "2.3.4.5",
+                    "port": 1337
+                }
             ]
         }
     ]
-
+            
     You can then select the items that you want to display.
-
+            
     .NOTES
     Author:     Luke Leigh
     Website:    https://blog.lukeleigh.com/
@@ -58,11 +62,11 @@ function Get-MerakiAccessPolicy {
 
     .LINK
     https://github.com/BanterBoy/CiscoMeraki/wiki
-
 #>
-
+            
+function Get-MerakiAccessPolicy {
     [CmdletBinding()]
-    
+            
     param(
         [Parameter(Mandatory = $True,
             ValueFromPipeline = $True,
@@ -85,7 +89,7 @@ function Get-MerakiAccessPolicy {
         $Uri = @{
             "accessPolicies" = "https://api.meraki.com/api/v0/networks/$NetworkID/accessPolicies"
         }
-
+        
         $accessPolicies = Invoke-RestMethod -Method GET -Uri $Uri.accessPolicies -Headers @{
             'X-Cisco-Meraki-API-Key' = "$ApiKey"
             'Content-Type'           = 'application/json'
